e288496f7708593f945602161c37b15ed4c8605f
pixiee534 <pixiee345@gmail.com>
2024-09-18 17:59:01 +0530

    Lesson6-Breif On JavaScript

---
+++ conditonstatements.js
@@ -0,0 +1,27 @@
+var digit= 1;
+
+var answer;
+
+if (digit<25){
+
+answer="LOW VALUE";
+
+}
+
+else if (digit>25){
+
+answer="HIGH VALUE";
+
+}
+
+else {
+
+answer="NO VALUE";
+
+}
+
+console.log (answer);
+    
+    
+
+
---
+++ datatypes.js
@@ -0,0 +1,17 @@
+var a=8;
+var b=12;
+var c=null;
+var d;
+{
+    let e= 3;
+    const f=2;
+    console.log("The Value of e is:" +e);
+console.log("br")
+console.log("The Value of f is:" +f);
+}
+console.log("a and b are equal : " +(a=b));
+console.log("br")
+console.log("The Value of c is:" +c);
+console.log("br")
+console.log("The Value of d is:" +d);
+console.log("br")
